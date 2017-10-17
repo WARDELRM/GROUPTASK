32ba59e84abe8c3d8f5d13822ed8f85531adb812
WARDELRM <32824420+WARDELRM@users.noreply.github.com>
2017-10-17 22:29:15 +1100

    A bunch of new stuff
    new map, music and base orc and skeleton enimies

--- js/grouptaskgame.js
+++ js/grouptaskgame.js
@@ -1,19 +1,26 @@
 window.onload = function() {
 
-  var game = new Phaser.Game (800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+  var game = new Phaser.Game (800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
   var map;
   var layer;
   var player;
+  var orc;
+  var skeleton;
   var moveKeys;
+  var playerHealth = 100;
+  var orcWave = 5;
+  var skeletonWave = 2;
 
   function preload() {
     game.load.spritesheet('wizard', 'assets/sprites/spritesheets/thewizard.png', 64, 64);
-    game.load.tilemap('testarena', 'assets/tilemaps/maps/testarenatiles.json', null, Phaser.Tilemap.TILED_JSON);
-    //game.load.tilemap('arena', 'assets/tilemaps/maps/arenatiles.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('orc', 'assets/sprites/spritesheets/orc.png', 64, 64);
+    game.load.spritesheet('skeleton', 'assets/sprites/spritesheets/skeleton.png', 64, 64);
+    game.load.tilemap('arena', 'assets/tilemaps/maps/arena2.0.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'assets/tilemaps/tiles/deserttiles.png');
-    game.load.image('testweapon', 'assets/sprites/testweapon.png')
+    game.load.image('testweapon', 'assets/sprites/testweapon.png');
     game.load.audio('music', 'assets/audio/music/The Elder Scrolls V Skyrim - Battle Music [REMASTERED].mp3');
+    game.load.audio('testfireball', 'assets/audio/sounds/testfireball.mp3');
 
   }
 
@@ -22,14 +29,14 @@ window.onload = function() {
     //Enable the Phaser ARCADE physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    map = game.add.tilemap('testarena');
-    map.addTilesetImage('Desert', 'tiles');
+    map = game.add.tilemap('arena');
+    map.addTilesetImage('arenatiles', 'tiles');
     layer = map.createLayer('Ground')
     layer.resizeWorld();
 
     music = game.add.audio('music');
       music.play('', 0, 1, true);
-      music.volume = 1;
+      music.volume = 0.1;
 
     player = game.add.sprite(game.world.centerY, game.world.centerX, 'wizard');
     game.physics.arcade.enable(player);
@@ -48,6 +55,14 @@ window.onload = function() {
     weapon.fireRate = 500;
     weapon.bullerRotateToVelocity = true;
     weapon.trackSprite(player);
+
+    orcs = game.add.group();
+    orcs.enableBody = true;
+    spawnOrcWave(orcWave);
+
+    skeletons = game.add.group();
+    skeletons.enableBody = true;
+    spawnSkeletonWave(skeletonWave);
 
     moveKeys = game.input.keyboard.addKeys(
       {
@@ -71,6 +86,10 @@ window.onload = function() {
 
     //Collision events
     //game.physics.arcade.overlap(player fireballs hitting a specific enemy type)
+    //game.physics.arcade.collide(orcs);
+    game.physics.arcade.collide(player, orcs, orcContact);
+    game.physics.arcade.collide(player, skeletons, skeletonContact);
+
 
     if(moveKeys.left.isDown) {
       player.x -= 4;
@@ -100,16 +119,95 @@ window.onload = function() {
       player.frame = 130;
       }
 
+      if (orc.x > player.x + 1) {
+        orc.x -= 2;
+        orc.animations.play('left');
+      } else if (orc.x < player.x - 1) {
+        orc.x += 2;
+        orc.animations.play('right');
+      } else {
+        if (orc.y > player.y) {
+          orc.animations.play('up');
+        } else {
+          orc.animations.play('down');
+        }
+      }
+      if (orc.y > player.y + 1) {
+        orc.y -= 2;
+      } else if (orc.y < player.y - 1) {
+        orc.y += 2;
+      }
 
+      if (skeleton.x > player.x + 200) {
+        skeleton.x -= 3;
+        skeleton.animations.play('left');
+      } else if (skeleton.x < player.x - 200) {
+        skeleton.x += 3;
+        skeleton.animations.play('right');
+      } else {
+        if (skeleton.y > player.y) {
+          skeleton.animations.play('up');
+        } else {
+          skeleton.animations.play('down');
+        }
+      }
+      if (skeleton.y > player.y + 200) {
+        skeleton.y -= 3;
+      } else if (skeleton.y < player.y - 200) {
+        skeleton.y += 3;
+      }
 
 
   }
 
   function spellCast() {
     player.animations.play('spellcast');
+    /*fireballSound = game.add.audio('testfireball');
+    fireballSound.play();
+    fireballSound.volume = 0.5;*/
   }
 
+  function spawnOrcWave(numSpawn) {
+    for (var i = 0; i < numSpawn; i++)
+    {
+      orc = orcs.create(player.x + Math.random() * 905 + 805, game.world.centerY, 'orc');
+      orc.anchor.setTo(0.5,0.5);
+      orc.animations.add('left', [217, 218, 219, 220, 221, 222, 223, 224], 10, true);
+      orc.animations.add('right', [265, 266, 267, 268, 269, 270, 271, 272], 10, true);
+      orc.animations.add('up', [105, 106, 107, 108, 109, 110, 111, 112], 10, true);
+      orc.animations.add('down', [241, 242, 243, 244, 245, 246, 247, 248], 10, true);
+    }
+  }
 
+  function spawnSkeletonWave(numSpawn) {
+    for (var i = 0; i < numSpawn; i++)
+    {
+      skeleton = skeletons.create(player.x - Math.random() * 905 - 805, game.world.centerY, 'skeleton');
+      skeleton.anchor.setTo(0.5,0.5);
+      skeleton.animations.add('left', [118, 119, 120, 121, 122, 123, 124, 125], 10, true);
+      skeleton.animations.add('right', [144, 145, 146, 147, 148, 149, 150, 151], 10, true);
+      skeleton.animations.add('up', [105, 106, 107, 108, 109, 110, 111, 112], 10, true);
+      skeleton.animations.add('down', [131, 132, 133, 134, 135, 136, 137, 138], 10, true);
+    }
+  }
+
+  function orcContact() {
+    console.log("orc contact");
+  }
+
+  function skeletonContact() {
+    console.log("skeleton contact");
+  }
+
+  function render() {
+    game.debug.body(player);
+    orcs.forEach(function(temp){
+      game.debug.body(temp);
+    }, this);
+    skeletons.forEach(function(temp2){
+      game.debug.body(temp2);
+    }, this);
+  }
 
 
 };
